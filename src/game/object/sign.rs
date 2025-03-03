492537c8d45d833da5ccc3696aca45385443633d
Rratic <Rratic@163.com>
2025-03-03 22:12:05 +0800

    feat: working example for sign message

--- src/game/object/sign.rs
+++ src/game/object/sign.rs
@@ -4,13 +4,11 @@ use crate::game::object::Object;
 #[derive(Reflect, Component, Clone)]
 #[reflect(Object, Component)]
 #[type_path = "sim::object"]
-pub struct SignStand {
-    pub mode: usize,
-}
+pub struct SignStand(usize);
 
 impl Object for SignStand {
     fn texture_info(&self) -> Option<(&str, Option<usize>)> {
-        Some(("sign", Some(self.mode)))
+        Some(("sign", Some(self.0)))
     }
 
     fn add_physics_components(&self, commands: &mut EntityCommands) {
--- src/message.rs
+++ src/message.rs
@@ -30,29 +30,26 @@ pub struct InfoText;
 // todo: add duration
 fn read_message_event(
     mut reader: EventReader<MessageEvent>,
-    mut query_text: Query<&mut Text, With<InfoText>>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    commands.spawn((
-        Text::new(""),
-        TextFont {
-            font: asset_server.load("fonts/open-sans.regular.ttf"),
-            font_size: 20.0,
-            ..default()
-        },
-        TextColor(bevy::color::palettes::css::DARK_GRAY.into()),
-        Node {
-            position_type: PositionType::Absolute,
-            top: Val::Px(5.0),
-            right: Val::Px(5.0),
-            ..default()
-        },
-        InfoText,
-    ));
     for ev in reader.read() {
-        let mut text = query_text.single_mut();
-        // todo: move
-        text.0 = ev.message.clone();
+        commands.spawn((
+            Text::new(ev.message.as_str()),
+            TextFont {
+                font: asset_server.load("fonts/open-sans.regular.ttf"),
+                font_size: 20.0,
+                ..default()
+            },
+            TextColor(bevy::color::palettes::css::DARK_GRAY.into()),
+            Node {
+                position_type: PositionType::Absolute,
+                top: Val::Px(5.0),
+                right: Val::Px(5.0),
+                ..default()
+            },
+            BackgroundColor(Color::WHITE),
+            InfoText,
+        ));
     }
 }
