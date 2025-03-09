da603001b9836adfc4642f5baac62f9139c80572
Rratic <Rratic@163.com>
2025-03-09 16:24:21 +0800

    refactor: add mod item_control

---
+++ src/game/item_control.rs
@@ -0,0 +1,72 @@
+use super::{inventory::*, item::*, object::*};
+use crate::{assets::*, character::*};
+use bevy::prelude::*;
+
+fn reach_inventory_item_then<F>(world: &World, inv: Entity, index: usize, f: F)
+where
+    F: FnOnce(&dyn Item, Entity) -> (),
+{
+    let storage = world.entity(inv).get::<ItemStorage>().unwrap();
+    let item = if let Some(item) = storage.get_index(index) {
+        item
+    } else {
+        return;
+    };
+
+    let type_registry = world.resource::<AppTypeRegistry>();
+    IsItem::inspect_then(world, item, type_registry, |guarded| {
+        f(guarded, item);
+    });
+}
+
+pub fn item_use(mut commands: Commands, world: &World, inventory: Res<Inventory>) {
+    let inv = inventory.bind.unwrap();
+    let index = inventory.selected;
+
+    let rpg_folder = world.resource::<RpgTextures>();
+
+    let position = world.resource::<ActorPosition>();
+    let target = world.resource::<crate::physics::camera::CursorCoords>();
+    reach_inventory_item_then(world, inv, index, |guarded, item| {
+        if guarded.check_can_use() {
+            guarded.item_use(
+                &mut commands,
+                item,
+                position.center + position.primary_hand_offset,
+                target.0,
+                rpg_folder,
+            );
+        }
+    });
+}
+
+pub fn item_modify(mut commands: Commands, world: &World, inventory: Res<Inventory>) {
+    let inv = inventory.bind.unwrap();
+    let index = inventory.selected;
+    reach_inventory_item_then(world, inv, index, |guarded, item| {
+        if guarded.check_can_modify() {
+            guarded.item_modify(&mut commands, item);
+        }
+    });
+}
+
+pub fn item_throw(mut commands: Commands, world: &World, inventory: Res<Inventory>) {
+    let inv = inventory.bind.unwrap();
+    let storage = world.entity(inv).get::<ItemStorage>().unwrap();
+    let index = inventory.selected;
+    if storage.view_count(index) < 1.0 {
+        return;
+    }
+
+    commands
+        .entity(inv)
+        .queue(move |mut entity: EntityWorldMut| {
+            let mut storage = entity.get_mut::<ItemStorage>().unwrap();
+            storage.extract_one(index.clone());
+        });
+
+    let it = world
+        .entity(storage.get_index(index).unwrap())
+        .get::<IsObject>()
+        .unwrap();
+}
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,14 +1,16 @@
-use crate::{assets::RpgTextures, character::*, control::*, scene::*, state::*};
-use avian2d::prelude::*;
+use crate::{character::*, control::*, scene::*, state::*};
 use bevy::prelude::*;
 
-use item::*;
-
 pub mod character;
-use character::*;
-
 pub mod inventory;
+pub mod item;
+pub mod item_control;
+pub mod mob;
+pub mod object;
+
+use character::*;
 use inventory::*;
+use item_control::*;
 
 pub struct GamePlugin;
 
@@ -61,42 +63,23 @@ impl Plugin for GamePlugin {
     }
 }
 
-pub mod item;
-pub mod mob;
-pub mod object;
-
 /* Handling inputs. */
 
 fn inputs_use(
-    mut commands: Commands,
+    commands: Commands,
     world: &World,
     keys: Res<ButtonInput<KeyCode>>,
     click: Res<ButtonInput<MouseButton>>,
     control_settings: Res<ControlSettings>,
     inventory: Res<Inventory>,
-    position: Res<ActorPosition>,
-    coords: Res<crate::physics::camera::CursorCoords>,
-    rpg_folder: Res<RpgTextures>,
 ) {
     if click.just_pressed(MouseButton::Left) || control_settings.check(ControlCode::Use, &keys) {
-        let inv = inventory.bind.unwrap();
-        let index = inventory.selected;
-        reach_inventory_item_then(world, inv, index, |guarded, item| {
-            if guarded.check_can_use() {
-                guarded.item_use(
-                    &mut commands,
-                    item,
-                    position.center + position.primary_hand_offset,
-                    coords.0,
-                    rpg_folder.as_ref(),
-                );
-            }
-        });
+        item_use(commands, world, inventory);
     }
 }
 
 fn inputs_modify(
-    mut commands: Commands,
+    commands: Commands,
     world: &World,
     keys: Res<ButtonInput<KeyCode>>,
     click: Res<ButtonInput<MouseButton>>,
@@ -105,58 +88,20 @@ fn inputs_modify(
 ) {
     if click.just_pressed(MouseButton::Right) || control_settings.check(ControlCode::Modify, &keys)
     {
-        let inv = inventory.bind.unwrap();
-        let index = inventory.selected;
-        reach_inventory_item_then(world, inv, index, |guarded, item| {
-            if guarded.check_can_modify() {
-                guarded.item_modify(&mut commands, item);
-            }
-        });
+        item_modify(commands, world, inventory);
     }
 }
 
 fn inputs_throw(
-    mut commands: Commands,
+    commands: Commands,
     world: &World,
     keys: Res<ButtonInput<KeyCode>>,
     control_settings: Res<ControlSettings>,
     inventory: Res<Inventory>,
 ) {
     if control_settings.check(ControlCode::Throw, &keys) {
-        let inv = inventory.bind.unwrap();
-        let storage = world.entity(inv).get::<ItemStorage>().unwrap();
-        let index = inventory.selected;
-        if storage.view_count(index) < 1.0 {
-            return;
-        }
-        commands
-            .entity(inv)
-            .queue(move |mut entity: EntityWorldMut| {
-                let mut storage = entity.get_mut::<ItemStorage>().unwrap();
-                storage.extract_one(index.clone());
-            });
-        commands.spawn((
-            // clone item here
-            RigidBody::Dynamic,
-        ));
+        item_throw(commands, world, inventory);
     }
-}
-
-fn reach_inventory_item_then<F>(world: &World, inv: Entity, index: usize, f: F)
-where
-    F: FnOnce(&dyn Item, Entity) -> (),
-{
-    let storage = world.entity(inv).get::<ItemStorage>().unwrap();
-    let item = if let Some(item) = storage.get_index(index) {
-        item
-    } else {
-        return;
-    };
-
-    let type_registry = world.resource::<AppTypeRegistry>();
-    IsItem::inspect_then(world, item, type_registry, |guarded| {
-        f(guarded, item);
-    });
 }
 
 fn inputs_number(
